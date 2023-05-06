4b56767d214e0f5af3532814cd6ae5df61ffa5fd
lashasuxa <lashasukhashvili1995@gmail.com>
2023-05-06 02:02:43 -0400

    response handler finished

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import Moon from "/icon-moon.svg"
 
 function App() {
   const [search, setSearch] = useState('');
-  const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState(null);
   const [theme, setTheme] = useState('DARK');
+  const [error, setError] = useState(false); // Add error state
 
   const handleChange = (e) => {
     setSearch(e.target.value);
@@ -15,29 +16,30 @@ function App() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError(false); // Reset error state on new search
     try {
       const response = await axios.get(`https://api.github.com/users/${search}`);
       setUserData(response.data);
       console.log(response.data)
     } catch (error) {
       console.error(error);
+      console.log(error)
+      setUserData(null);
+      setError(true); // Set error state on error
     }
   };
   const toggleTheme = () => {
     setTheme(theme === 'DARK' ? 'LIGHT' : 'DARK');
   };
-  // function formatDate(dateString) {
-  //   const date = new Date(dateString);
-  //   const formatter = new Intl.DateTimeFormat('en-US', {
-  //     year: 'numeric',
-  //     month: 'short',
-  //     day: '2-digit'
-  //   });
-  
-  //   const formattedDate = formatter.format(date);
-  
-  //   return `Joined ${formattedDate}`;
-  // }
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.toLocaleString('en-US', { month: 'short' });
+    const year = date.getFullYear();
+
+    return ` ${day} ${month} ${year}`;
+  }
   
 
   return (
@@ -57,58 +59,63 @@ function App() {
           value={search}
           onChange={handleChange}
         />
+        {error && <span className="error-message">No results</span>} 
         <button type="submit">Search</button>
       </form>
       
+      {userData ? (
         <div className="card">
           <div className="card-top">
-          <img src={userData.avatar_url} alt="" />
-          <div className="card-top-middle">
-            <h2 className="name">{userData.name}</h2>
-            <span className="link">{userData.login}</span>
-            <p className="bio">{userData.bio ? userData.bio:"This profile has no bio" }</p>
-          </div>
-              <div className="card-top-right">
-                <span className="date">hii</span>
-              </div>
+            <img src={userData.avatar_url} alt="" />
+            <div className="card-top-middle">
+              <h2 className="name">{userData.name}</h2>
+              <span className="link">{userData.login}</span>
+              <p className="bio">{userData.bio ? userData.bio : "This profile has no bio"}</p>
+            </div>
+            <div className="card-top-right">
+              <span className="date">Joined {formatDate(userData.created_at)}</span>
+            </div>
           </div>
           <div className="card_middle">
-            <div className="repos_container">
+            <div className="repos_container column">
               <span className='reposText'>Repos</span>
               <span className="reposNum">{userData.public_repos}</span>
             </div>
-            <div className="followers_container">
-                <span className="followersText">Followers</span>
-                <span className="followersNum">{userData.followers}</span>
+            <div className="followers_container column">
+              <span className="followersText">Followers</span>
+              <span className="followersNum">{userData.followers}</span>
             </div>
-            <div className="following_container">
+            <div className="following_container column">
               <span className="followingText">Following</span>
               <span className="followingNum">{userData.following}</span>
             </div>
           </div>
-            <div className="card_bottom">
-              <div className="location_twitter">
-                  <div className="location_container">
-                    <img src="/icon-location.svg" alt="" />
-                    <span className="location">{userData.location}</span>
-                  </div>
-                  <div className="tweeter-container">
-                    <img src="/icon-twitter.svg" alt="" />
-                    <span className="tweeter">{userData.twitter_username}</span>
-                  </div>
+          <div className="card_bottom">
+            <div className="bottom-left">
+              <div className="location_container">
+                <img src="/icon-location.svg" alt="" />
+                <span className="location">{userData.location}</span>
               </div>
-                <div className="website_company">
-                  <div className="website_container">
+                <div className="website_container">
                     <img src="/icon-website.svg" alt="" />
-                    <span className="site">{userData.blog}</span>
-                  </div>
-                  <div className="company_container">
-                    <img src="/icon-company.svg" alt="" />
-                    <span className="company">{userData.company}</span>
-                  </div>
+                    <span className="site">{userData.blog? userData.blog:"Not Available"}</span>
                 </div>
             </div>
+            <div className="bottom-right">
+
+              <div className="tweeter-container">
+                <img src="/icon-twitter.svg" alt="" />
+                <span className="tweeter">{userData.twitter_username ? userData.twitter_username : "Not Available"}</span>
+              </div>
+
+              <div className="company_container">
+                <img src="/icon-company.svg"  alt="" />
+                <span className="company">{userData.company ? userData.company:"Not Available"}</span>
+              </div>
+            </div>
+          </div>
         </div>
+      ) : null}
       
     </main>
   );
